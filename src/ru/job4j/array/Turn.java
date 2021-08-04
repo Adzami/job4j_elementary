d521878251f207660dc19282bcf0e2b513000558
sklopkov <aeol79@gmail.com>
2021-08-04 13:53:21 +0300

    6.2.3. Перевернуть массив. [#226790] с мусорной переменной

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class Turn {
     public static int[] back(int[] array) {
         for (int i = 0; i < (array.length / 2); i++) {
-            array[i] += array[array.length - 1 - i];
-            array[array.length - 1 - i] = array[i] - array[array.length - 1 - i];
-            array[i] = array[i] - array[array.length - 1 - i];
+            int trashTmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = trashTmp;
         }
         return array;
     }
